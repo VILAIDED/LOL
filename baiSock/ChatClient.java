9770fae9e421447824a6a2a48591da118cd9100f
VILAIDED <65062161+VILAIDED@users.noreply.github.com>
2021-12-01 22:12:14 +0700

    Update ChatClient.java

--- baiSock/ChatClient.java
+++ baiSock/ChatClient.java
@@ -1,46 +1,52 @@
-package baiSock;
+package Bai5;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ChatClient {
+public class ChatRoomClient {
+
 	public static void main(String[] args) {
-		new ChatClient();
+		new ChatRoomClient();
 	}
-	public ChatClient() {
+
+	public ChatRoomClient() {
 		try {
-		Socket sock = new Socket("42.116.159.207",80);
-		DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
-		DataInputStream dis = new DataInputStream(sock.getInputStream());
-		dos.writeUTF("Manivong litar");
-		new ClientXuly(sock).start();
-		Scanner sc = new Scanner(System.in);
-	
-		while(true) {
-			String msg = sc.nextLine();
-			dos.writeUTF(msg);
-		}
-		}catch(Exception ex) {
-			
+			Socket soc = new Socket("localhost", 80);
+			DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+			DataInputStream dis = new DataInputStream(soc.getInputStream());
+			dos.writeUTF("Pham Minh Tuan");
+			new ClientXuly(soc).start();
+			Scanner sc = new Scanner(System.in);
+			while (true) {
+				// Gui msg cho server
+				String msg = sc.nextLine();
+				dos.writeUTF(msg);
+				// Code sai!!!
+				// System.out.println(dis.readUTF());
+			}
+		} catch (Exception e) {
 		}
 	}
 }
-class ClientXuly extends Thread{
-	Socket sock;
-	public ClientXuly(Socket sock) {
-		this.sock = sock;	
+
+class ClientXuly extends Thread {
+	Socket soc;
+
+	public ClientXuly(Socket soc) {
+		this.soc = soc;
 	}
+
 	public void run() {
 		try {
-			DataInputStream dis = new DataInputStream(sock.getInputStream());
-			while(true) {
+			DataInputStream dis = new DataInputStream(soc.getInputStream());
+			while (true) {
 				System.out.println(dis.readUTF());
 			}
-		}catch(Exception ex) {
-			
+		} catch (Exception e) {
+
 		}
 	}
-	
 }
