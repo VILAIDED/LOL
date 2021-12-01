9ef810358428765bf9126f6ab04b56250cc49d49
VILAIDED <65062161+VILAIDED@users.noreply.github.com>
2021-12-01 22:11:46 +0700

    Update ChatServer.java

--- baiSock/ChatServer.java
+++ baiSock/ChatServer.java
@@ -1,4 +1,4 @@
-package baiSock;
+package Bai5;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -6,8 +6,51 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
 
-public class ChatServer {
+public class ChatRoomServer {
 	public static void main(String[] args) {
-		System.out.println(0%2);
+		new ChatRoomServer();
 	}
+	
+	static Vector<ChatXuly> clients = new Vector<ChatXuly>();
+
+	public ChatRoomServer() {
+		try {
+			ServerSocket server = new ServerSocket(80);
+			while (true) {
+				Socket soc = server.accept();
+				ChatXuly c = new ChatXuly(soc);
+				clients.add(c);
+				c.start();
+			}
+		} catch (Exception e) {
+		}
 	}
+}
+
+class ChatXuly extends Thread {
+	Socket soc;
+	DataInputStream dis;
+	DataOutputStream dos;
+	String name;
+	public ChatXuly(Socket soc) {
+		try {
+			this.soc = soc;
+			dis = new DataInputStream(soc.getInputStream());
+			dos = new DataOutputStream(soc.getOutputStream());
+		} catch (Exception e) {
+
+		}
+	}
+	public void run() {
+		try {
+			name = dis.readUTF();
+			while (true) {
+				String msg = dis.readUTF();
+				for (ChatXuly c : ChatRoomServer.clients) {
+					c.dos.writeUTF(name+soc.getInetAddress()+">"+msg);
+				}
+			}
+		}catch(Exception e) {
+		}
+	}
+}
